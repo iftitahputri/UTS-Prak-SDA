7c028e8cbb85af225992b99dcf9262f127e079c3
Iftitah Putri Syahna <24iftitah@mhs.usk.ac.id>
2025-03-23 10:16:01 +0700

    infix to postfix

--- parsing_expression.c
+++ parsing_expression.c
@@ -7,6 +7,8 @@
 char stack[MAX][MAX];  
 int top = -1;
 
+void inToPost();
+
 void pushStr(char *);
 char *popStr();
 char peek();
@@ -15,6 +17,48 @@ int precedence(char);
 int space(char);
 int isOperator(char);
 int isOperand(char);
+
+int main (){
+
+    return 0;
+}
+
+void inToPost() {
+    char infix[MAX], postfix[MAX], symbol, temp[MAX];
+    int i, j = 0;
+    top = -1;
+    
+    printf("Masukkan ekspresi infix: ");
+    fgets(infix, MAX, stdin);
+    if (infix[strlen(infix) - 1] == '\n') 
+        infix[strlen(infix) - 1] = '\0';
+
+    for (i = 0; i < strlen(infix); i++) {
+        symbol = infix[i];
+        if (!space(symbol) && isOperand(symbol)) {
+            postfix[j++] = symbol;
+        } else if (!space(symbol) && isOperator(symbol)) {
+            while (!isEmpty() && precedence(peek()) >= precedence(symbol)) {
+                postfix[j++] = popStr()[0];
+            }
+            snprintf(temp, MAX, "%c", symbol);
+            pushStr(temp);
+        } else if (symbol == '(') {
+            snprintf(temp, MAX, "%c", symbol);
+            pushStr(temp);
+        } else if (symbol == ')') {
+            while (!isEmpty() && peek() != '(') {
+                postfix[j++] = popStr()[0];
+            }
+            popStr();
+        }
+    }
+    while (!isEmpty()) {
+        postfix[j++] = popStr()[0];
+    }
+    postfix[j] = '\0';
+    printf("Postfix = %s\n", postfix);
+}
 
 void pushStr(char *str) {
     if (top < MAX - 1) {
